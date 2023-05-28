0385f52074b241cd27e29fbb5711af69c35ff8c4
Ivan Shirykhalov <ivanshirykhalov@gmail.com>
2023-05-28 20:01:44 +0300

    projects

--- src/Projects/Projects.tsx
+++ src/Projects/Projects.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import s from './Projects.module.css'
+import s from './Projects.module.scss'
 import sContainer from "../common/styles/Container.module.css";
 import {Project} from "./Project/Project";
 import {Title} from "../components/title/Title";
